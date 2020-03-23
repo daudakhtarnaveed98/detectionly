7cdcd1667f8d7d4ff8c772964889a4891b245ab4
Daud Akhtar Naveed <daudakhtarnaveed@gmail.com>
2020-03-23 06:24:25 +0500

    Add route protection

--- backend/api/registry/graphql/resolvers/mutation/mapper.js
+++ backend/api/registry/graphql/resolvers/mutation/mapper.js
@@ -4,6 +4,9 @@
 const update = require("./update");
 const deletion  = require("./deletion");
 const registration = require("./registration");
+const jsonwebtoken = require("jsonwebtoken");
+const commons = require("../../../../../commons");
+require("dotenv-expand")(require("dotenv").config());
 
 // Resolvers map.
 const mutationResolversMap = {
@@ -16,31 +19,62 @@ const mutationResolversMap = {
         }
     },
 
-    updateUserInformation: async (args) => {
-        const {userEmailAddress: userEmailAddress, updatedInformation: updatedInformation} = args;
+    updateUserInformation: async (args, req) => {
+        // Get json web token from authorization header.
+        const jwt = req.headers['authorization'].split(" ")[1];
+
+        // Verify the provided token.
         try {
-            return await update.updateUserInformation(userEmailAddress, updatedInformation);
+            jsonwebtoken.verify(jwt, process.env.PRIVATE_KEY);
+            const {userEmailAddress: userEmailAddress, updatedInformation: updatedInformation} = args;
+            try {
+                return await update.updateUserInformation(userEmailAddress, updatedInformation);
+            } catch (error) {
+                console.error(error);
+            }
         } catch (error) {
             console.error(error);
+            return {statusCode: commons.statusCodes.UNAUTHORIZED, statusMessage: "UNAUTHORIZED", responseMessage: "Invalid / Expired Token"};
         }
     },
 
-    updateUserPassword: async (args) => {
-        const {userEmailAddress: userEmailAddress, userUpdatePasswordData: userUpdatePasswordData} = args;
-        const {currentPassword: currentPassword, newPassword: newPassword} = userUpdatePasswordData;
+    updateUserPassword: async (args, req) => {
+        // Get json web token from authorization header.
+        const jwt = req.headers['authorization'].split(" ")[1];
+
+        // Verify the provided token.
         try {
-            return await update.updateUserPassword(userEmailAddress, currentPassword, newPassword);
+            jsonwebtoken.verify(jwt, process.env.PRIVATE_KEY);
+            const {userEmailAddress: userEmailAddress, userUpdatePasswordData: userUpdatePasswordData} = args;
+            const {currentPassword: currentPassword, newPassword: newPassword} = userUpdatePasswordData;
+            try {
+                return await update.updateUserPassword(userEmailAddress, currentPassword, newPassword);
+            } catch (error) {
+                console.error(error);
+            }
         } catch (error) {
             console.error(error);
+            return {statusCode: commons.statusCodes.UNAUTHORIZED, statusMessage: "UNAUTHORIZED", responseMessage: "Invalid / Expired Token"};
         }
+
     },
 
-    deleteUserAccount: async (args) => {
-        const {userEmailAddress: userEmailAddress, password: password} = args;
+    deleteUserAccount: async (args, req) => {
+        // Get json web token from authorization header.
+        const jwt = req.headers['authorization'].split(" ")[1];
+
+        // Verify the provided token.
         try {
-            return await deletion.deleteUserAccount(userEmailAddress, password);
+            jsonwebtoken.verify(jwt, process.env.PRIVATE_KEY);
+            const {userEmailAddress: userEmailAddress, password: password} = args;
+            try {
+                return await deletion.deleteUserAccount(userEmailAddress, password);
+            } catch (error) {
+                console.error(error);
+            }
         } catch (error) {
             console.error(error);
+            return {statusCode: commons.statusCodes.UNAUTHORIZED, statusMessage: "UNAUTHORIZED", responseMessage: "Invalid / Expired Token"};
         }
     }
 };
