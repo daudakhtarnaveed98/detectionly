5762feaab760953691d8b05e7b74454f9340f808
Daud Akhtar Naveed <daudakhtarnaveed@gmail.com>
2020-04-05 19:07:11 +0500

    Add utils to frontend

---
+++ frontend/src/utils/index.js
@@ -0,0 +1,6 @@
+// Import modules.
+import * as utils from "./utils"
+
+// Export.
+export const validateEmail = utils.emailValidator;
+export const validatePassword = utils.passwordValidator;
---
+++ frontend/src/utils/utils.js
@@ -0,0 +1,10 @@
+// Utility functions.
+export const emailValidator = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
+
+export const passwordValidator = (password) => {
+  const re = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  return re.test(String(password));
+};
